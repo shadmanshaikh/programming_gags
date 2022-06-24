882d106d57a89238bbc00a7b4f24bcc9deddcf32
shady4real <shadmanforreal45@gmail.com>
2022-06-24 12:03:15 +0530

    for all files commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+async function loadNames() {
+    var shd = "shadman";
+    const response = await fetch('https://v2.jokeapi.dev/joke/Programming');
+    const names = await response.json();
+    // logs [{ name: 'Joker'}, { name: 'Batman' }]
+    console.log(names.delivery,names.category);
+    document.getElementById("info").innerHTML = "Joke: "+names.delivery +'<br>' + "Category: "+names.category +'<br>'+"author: "+shd;
+    document.getElementById("info").style.border = "2px solid blue";
+  }
+loadNames();
+
